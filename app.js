5feb6e377df0a6f7b4d3bad8c307ff7c47bab663
Başak Seçmen <basaksecmen@gmail.com>
2019-09-26 14:29:30 +0300

    route position change test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const compareRoutes = require('./routes/compare');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/api/compare/:coin/:currency', compareRoutes);
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
     })
 
 }
-
+app.get('/api/compare/:coin/:currency', compareRoutes);
 
 
 module.exports = app;
