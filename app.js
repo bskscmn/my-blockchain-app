e83ea71058453207f3f8c9bd7c8a50a916a7591a
Başak Seçmen <basaksecmen@gmail.com>
2019-09-26 09:29:21 +0300

    controller bypass

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const compareRoutes = require('./routes/compare');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
@@ -18,7 +18,42 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 
-app.get('/api/compare/:coin/:currency', compareRoutes);
+app.get('/api/compare/:coin/:currency', (req,res) => {
+
+        let btc = [
+            {
+                bitfinex: {
+                    price: 0,
+                    bid: 0,
+                    ask: 0
+                },
+                bitstamp: {
+                    price: 0,
+                    bid: 0,
+                    ask: 0
+                },
+                kraken: {
+
+                    price: 0,
+                    bid: 0,
+                    ask: 0
+                },
+                bittrex: {
+                    price: 0,
+                    bid: 0,
+                    ask: 0
+                },
+                coinbase: {
+                    price: 0,
+                    bid: 0,
+                    ask: 0
+                }
+            }
+        ];
+
+        return res.status(200).json(btc);
+    }
+);
 
 module.exports = app;
 
